4f1f78932446a7ff725396be05675ef59d041d0c
Lowtrixx <80490749+Lowtrixx@users.noreply.github.com>
2022-12-04 20:42:46 +0100

    Delete securitycraft.js

--- kubejs/server_scripts/securitycraft.js
+++
@@ -1,4 +0,0 @@
-onEvent('recipes', event => {
-    const toRem = ['securitycraft:mine','securitycraft:bouncing_betty', 'securitycraft:claymore', 'securitycraft:ims', 'securitycraft:track_mine']
-    toRem.forEach(e => {event.remove({output:e})})
-})  
